c92da925f3a3e4615f301e6dde75afdb830d76f2
patelharsh93 <patelharshm93@gmail.com>
2015-06-12 11:19:21 -0400

    Added slotMachine assets

--- Comp397-My_Slot_Machine/Scripts/game.js
+++ Comp397-My_Slot_Machine/Scripts/game.js
@@ -1,88 +1,64 @@
-﻿/// <reference path="typings/stats/stats.d.ts" />
+/// <reference path="typings/stats/stats.d.ts" />
 /// <reference path="typings/easeljs/easeljs.d.ts" />
 /// <reference path="typings/tweenjs/tweenjs.d.ts" />
 /// <reference path="typings/soundjs/soundjs.d.ts" />
 /// <reference path="typings/preloadjs/preloadjs.d.ts" />
-
 //Game framework variable
-
 var canvas = document.getElementById("canvas");
-var stage: createjs.Stage;
-var stats: Stats;
-
-var assets: createjs.LoadQueue;
+var stage;
+var stats;
+var assets;
 var manifest = [
     { id: "background", src: "assets/images/slotMachine.png" },
     { id: "clicked", src: "assets/audio/Clicked.wav" }
 ];
-
 //Game variables
-var background: createjs.Bitmap
-
+var background;
 //preloaded Function
-function preload()
-{
+function preload() {
     assets = new createjs.LoadQueue();
     assets.installPlugin(createjs.Sound);
     //assets.on is an event Listener, triggers when assets are cmpletely loaded
-    assets.on("complete", init, this);  
-    assets.loadManifest(manifest);     //Manifest is assets manager, an array of object
-
+    assets.on("complete", init, this);
+    assets.loadManifest(manifest); //Manifest is assets manager, an array of object
     //Setup statics object
     setupStats();
 }
-
-
 //Callback function that intializies game objects
-function init(){
-    stage = new createjs.Stage(canvas);  //refrence to the stage
+function init() {
+    stage = new createjs.Stage(canvas); //refrence to the stage
     stage.enableMouseOver(20);
     //Ticker is a static class
     createjs.Ticker.setFPS(60); //framerate for the game
     //event listener triggers 60 ms times every second
     createjs.Ticker.on("tick", gameLoop);
-
     //Calling main Function
     main();
 }
-
-function setupStats()
-{
+function setupStats() {
     stats = new Stats();
     stats.setMode(2);
-
     // align bottom-right
     stats.domElement.style.position = 'absolute';
     stats.domElement.style.left = '330px';
     stats.domElement.style.top = '10px';
-
     document.body.appendChild(stats.domElement);
 }
-
 //Call back function that creates our main gameLoop- refresed 60 fps
-function gameLoop()
-{
+function gameLoop() {
     stats.begin(); //Begin measuring
-
     stage.update();
-
     stats.end(); //
 }
-
 //Call back function that allows me to respond to button clcik events
-function pinkButtonClicked(event: createjs.MouseEvent)
-{
+function pinkButtonClicked(event) {
     createjs.Sound.play("clicked");
 }
-
 //Call back function that changes the alpha transparency of the button
-
-
-
 //Our main function
 function main() {
-   //add in slot machine graphics
+    //add in slot machine graphics
     background = new createjs.Bitmap(assets.getResult("background"));
     stage.addChild(background);
-
 }
+//# sourceMappingURL=game.js.map
