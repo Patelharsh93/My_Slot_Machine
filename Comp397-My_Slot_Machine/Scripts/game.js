4827439216eb5aee28b9c8e892af40fc3f966566
patelharsh93 <patelharshm93@gmail.com>
2015-06-15 11:29:51 -0400

    wired up spin button functionality

--- Comp397-My_Slot_Machine/Scripts/game.js
+++ Comp397-My_Slot_Machine/Scripts/game.js
@@ -52,6 +52,17 @@ var atlas = {
 var background;
 var textureALtlas;
 var spinButton;
+/*Tally variables */
+var grapes = 0;
+var bananas = 0;
+var oranges = 0;
+var cherries = 0;
+var bars = 0;
+var bells = 0;
+var sevens = 0;
+var blanks = 0;
+var spinResult;
+var fruits = "";
 //preloaded Function
 function preload() {
     assets = new createjs.LoadQueue();
@@ -90,9 +101,65 @@ function gameLoop() {
     stage.update();
     stats.end(); //
 }
+/* Utility function to check if a value falls within a range of bounds */
+function checkRange(value, lowerBounds, upperBounds) {
+    if (value >= lowerBounds && value <= upperBounds) {
+        return value;
+    }
+    else {
+        return !value;
+    }
+}
+/* When this function is called it determines the betLine results.
+e.g. Bar - Orange - Banana */
+function Reels() {
+    var betLine = [" ", " ", " "];
+    var outCome = [0, 0, 0];
+    for (var spin = 0; spin < 3; spin++) {
+        outCome[spin] = Math.floor((Math.random() * 65) + 1);
+        switch (outCome[spin]) {
+            case checkRange(outCome[spin], 1, 27):
+                betLine[spin] = "blank";
+                blanks++;
+                break;
+            case checkRange(outCome[spin], 28, 37):
+                betLine[spin] = "Grapes";
+                grapes++;
+                break;
+            case checkRange(outCome[spin], 38, 46):
+                betLine[spin] = "Banana";
+                bananas++;
+                break;
+            case checkRange(outCome[spin], 47, 54):
+                betLine[spin] = "Orange";
+                oranges++;
+                break;
+            case checkRange(outCome[spin], 55, 59):
+                betLine[spin] = "Cherry";
+                cherries++;
+                break;
+            case checkRange(outCome[spin], 60, 62):
+                betLine[spin] = "Bar";
+                bars++;
+                break;
+            case checkRange(outCome[spin], 63, 64):
+                betLine[spin] = "Bell";
+                bells++;
+                break;
+            case checkRange(outCome[spin], 65, 65):
+                betLine[spin] = "Seven";
+                sevens++;
+                break;
+        }
+    }
+    return betLine;
+}
 //Call back function that allows me to respond to button clcik events
 function spinButtonClicked(event) {
     createjs.Sound.play("clicked");
+    spinResult = Reels();
+    fruits = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
+    console.log(fruits);
 }
 //Call back function that changes the alpha transparency of the button
 //Our main function
